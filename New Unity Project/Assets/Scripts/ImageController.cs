7c78ac39d63eadeeb95d43b68d0487d0020664c2
Tyler Hales <10869186@my.uvu.edu>
2019-09-04 14:36:48 -0600

    Image Component and Health bar update

---
+++ New Unity Project/Assets/Scripts/ImageController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Image))]
+
+public class ImageController : MonoBehaviour
+{
+    private Image imageComponent;
+    public UnityEvent UpdateImageEvent;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        imageComponent = GetComponent<Image>();
+    }
+
+    public void UpdateImageComponent(FloatData dataobj)
+    {
+        imageComponent.fillAmount = dataobj.value;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateImageEvent.Invoke();
+    }
+}
