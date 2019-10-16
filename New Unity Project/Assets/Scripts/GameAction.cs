bec2897a0964de5fdb62b255e1a53ead3f704378
Tyler Hales <10869186@my.uvu.edu>
2019-10-16 14:49:16 -0600

    Vector Data & Checkpoints

--- New Unity Project/Assets/Scripts/GameAction.cs
+++ New Unity Project/Assets/Scripts/GameAction.cs
@@ -6,10 +6,16 @@ using UnityEngine.Events;
 public class GameAction : ScriptableObject
 {
     public UnityAction action;
+    public UnityAction<Transform> transformAction;
 
     //Unity action cannot be called by other events. 
     public void Raise()
     {
         action?.Invoke();
+    }
+
+    public void Raise(Transform transformObj)
+    {
+        transformAction?.Invoke(transformObj);
     }
 }
