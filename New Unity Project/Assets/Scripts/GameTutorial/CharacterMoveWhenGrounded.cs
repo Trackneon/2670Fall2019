e8e761ac89cd91c39669a39818bb46f44ce5aa5a
Tyler Hales <10869186@my.uvu.edu>
2019-11-13 23:05:33 -0700

    11/13

---
+++ New Unity Project/Assets/Scripts/GameTutorial/CharacterMoveWhenGrounded.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterMoveWhenGrounded : CharacterBaseNew, IMove
+{
+
+    public override void Move(CharacterController controller)
+    {
+        if (Controller.isGrounded)
+        {
+            location.x = Input.GetAxis("Horizontal") * moveSpeed;
+        }
+        
+        location.y += gravity * Time.deltaTime;
+        Controller.Move(location * Time.deltaTime);
+    }
+}
